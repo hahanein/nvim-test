c7237aefabc3692f20e327c6cb478cf683b8ef17
Benjamin Westphal <hahanein@users.noreply.github.com>
2024-08-24 15:07:53 +0200

    port plugin script to lua

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,66 +1,113 @@
-if exists('g:loaded_test')
-  finish
-endif
-let g:loaded_test = 1
+-- Check if the plugin is already loaded
+if vim.g.loaded_test then
+	return
+end
+vim.g.loaded_test = 1
 
-let g:test#plugin_path = expand('<sfile>:p:h:h')
+-- Set the plugin path
+vim.g["test#plugin_path"] = vim.fn.expand("<sfile>:p:h:h")
 
-let g:test#default_runners = {
-  \ 'CSharp':     ['Xunit', 'DotnetTest'],
-  \ 'Clojure':    ['FireplaceTest', 'LeinTest'],
-  \ 'Crystal':    ['CrystalSpec'],
-  \ 'Cpp':        ['Catch2'],
-  \ 'Dart':       ['DartTest', 'FlutterTest'],
-  \ 'Elixir':     ['ExUnit', 'ESpec'],
-  \ 'Elm':        ['ElmTest'],
-  \ 'Erlang':     ['CommonTest', 'EUnit', 'PropEr'],
-  \ 'Go':         ['GoTest', 'Ginkgo', 'RichGo', 'Delve'],
-  \ 'Groovy':     ['MavenTest', 'GradleTest'],
-  \ 'Haskell':    ['StackTest', 'CabalTest'],
-  \ 'Java':       ['MavenTest', 'GradleTest'],
-  \ 'JavaScript': ['Ava', 'CucumberJS', 'DenoTest', 'Intern', 'TAP', 'Teenytest', 'Karma', 'Lab', 'Mocha',  'NgTest', 'Nx', 'Jasmine', 'Jest', 'ReactScripts', 'WebdriverIO', 'Cypress', 'VueTestUtils', 'Playwright', 'Vitest', 'Ember'],
-  \ 'Kotlin':     ['GradleTest'],
-  \ 'Lua':        ['Busted'],
-  \ 'Mint':       ['MintTest'],
-  \ 'Nim':        ['UnitTest'],
-  \ 'PHP':        ['Codeception', 'Dusk', 'Pest', 'PHPUnit', 'Behat', 'PHPSpec', 'Kahlan', 'Peridot'],
-  \ 'Perl':       ['Prove'],
-  \ 'Python':     ['Behave', 'DjangoTest', 'PyTest', 'PyUnit', 'Nose', 'Nose2', 'Mamba'],
-  \ 'Racket':     ['RackUnit'],
-  \ 'Ruby':       ['Rails', 'M', 'Minitest', 'RSpec', 'Cucumber', 'TestBench'],
-  \ 'Rust':       ['CargoNextest', 'CargoTest'],
-  \ 'Scala':      ['SbtTest', 'BloopTest'],
-  \ 'Shell':      ['Bats', 'ShellSpec'],
-  \ 'Swift':      ['SwiftPM'],
-  \ 'VimL':       ['Themis', 'VSpec', 'Vader', 'Testify', 'Vroom'],
-  \ 'Zig':        ['ZigTest'],
-  \ 'Gleam':      ['GleamTest'],
-\}
+-- Set default runners
+vim.g["test#default_runners"] = {
+	CSharp = { "Xunit", "DotnetTest" },
+	Clojure = { "FireplaceTest", "LeinTest" },
+	Crystal = { "CrystalSpec" },
+	Cpp = { "Catch2" },
+	Dart = { "DartTest", "FlutterTest" },
+	Elixir = { "ExUnit", "ESpec" },
+	Elm = { "ElmTest" },
+	Erlang = { "CommonTest", "EUnit", "PropEr" },
+	Go = { "GoTest", "Ginkgo", "RichGo", "Delve" },
+	Groovy = { "MavenTest", "GradleTest" },
+	Haskell = { "StackTest", "CabalTest" },
+	Java = { "MavenTest", "GradleTest" },
+	JavaScript = {
+		"Ava",
+		"CucumberJS",
+		"DenoTest",
+		"Intern",
+		"TAP",
+		"Teenytest",
+		"Karma",
+		"Lab",
+		"Mocha",
+		"NgTest",
+		"Nx",
+		"Jasmine",
+		"Jest",
+		"ReactScripts",
+		"WebdriverIO",
+		"Cypress",
+		"VueTestUtils",
+		"Playwright",
+		"Vitest",
+		"Ember",
+	},
+	Kotlin = { "GradleTest" },
+	Lua = { "Busted" },
+	Mint = { "MintTest" },
+	Nim = { "UnitTest" },
+	PHP = { "Codeception", "Dusk", "Pest", "PHPUnit", "Behat", "PHPSpec", "Kahlan", "Peridot" },
+	Perl = { "Prove" },
+	Python = { "Behave", "DjangoTest", "PyTest", "PyUnit", "Nose", "Nose2", "Mamba" },
+	Racket = { "RackUnit" },
+	Ruby = { "Rails", "M", "Minitest", "RSpec", "Cucumber", "TestBench" },
+	Rust = { "CargoNextest", "CargoTest" },
+	Scala = { "SbtTest", "BloopTest" },
+	Shell = { "Bats", "ShellSpec" },
+	Swift = { "SwiftPM" },
+	VimL = { "Themis", "VSpec", "Vader", "Testify", "Vroom" },
+	Zig = { "ZigTest" },
+	Gleam = { "GleamTest" },
+}
 
-let g:test#custom_strategies = get(g:, 'test#custom_strategies', {})
-let g:test#custom_transformations = get(g:, 'test#custom_transformations', {})
-let g:test#runner_commands = get(g:, 'test#runner_commands', [])
+-- Set custom strategies, transformations, and runner commands
+vim.g["test#custom_strategies"] = vim.tbl_get(vim.g, "test#custom_strategies") or {}
+vim.g["test#custom_transformations"] = vim.tbl_get(vim.g, "test#custom_transformations") or {}
+vim.g["test#runner_commands"] = vim.tbl_get(vim.g, "test#runner_commands") or {}
 
-command! -nargs=* -bar TestNearest call test#run('nearest', split(<q-args>))
-command! -nargs=* -bar -complete=file
-      \                TestFile    call test#run('file', split(<q-args>))
-command! -nargs=* -bar TestClass   call test#run('class', split(<q-args>))
-command! -nargs=* -bar TestSuite   call test#run('suite', split(<q-args>))
-command! -nargs=* -bar TestLast    call test#run_last(split(<q-args>))
-command!          -bar TestVisit   call test#visit()
+-- Define commands
+vim.api.nvim_create_user_command("TestNearest", function(opts)
+	vim.fn["test#run"]("nearest", vim.split(opts.args, "%s+"))
+end, { nargs = "*", bar = true })
 
-for [s:language, s:runners] in items(test#get_runners())
-  for s:runner in s:runners
-    if index(g:test#runner_commands, s:runner) != -1
-      if exists(':'.s:runner) | continue | endif
-      let s:runner_id = tolower(s:language).'#'.tolower(s:runner)
-      execute 'command! -bar -nargs=* -complete=file'
-            \ s:runner
-            \ 'call test#execute("'.s:runner_id.'", split(<q-args>))'
-    endif
-  endfor
-endfor
+vim.api.nvim_create_user_command("TestFile", function(opts)
+	vim.fn["test#run"]("file", vim.split(opts.args, "%s+"))
+end, { nargs = "*", bar = true, complete = "file" })
 
-if &autochdir
-  let g:test#project_root = getcwd()
-endif
+vim.api.nvim_create_user_command("TestClass", function(opts)
+	vim.fn["test#run"]("class", vim.split(opts.args, "%s+"))
+end, { nargs = "*", bar = true })
+
+vim.api.nvim_create_user_command("TestSuite", function(opts)
+	vim.fn["test#run"]("suite", vim.split(opts.args, "%s+"))
+end, { nargs = "*", bar = true })
+
+vim.api.nvim_create_user_command("TestLast", function(opts)
+	vim.fn["test#run_last"](vim.split(opts.args, "%s+"))
+end, { nargs = "*", bar = true })
+
+vim.api.nvim_create_user_command("TestVisit", function()
+	vim.fn["test#visit"]()
+end, { bar = true })
+
+-- Iterate through runners and define additional commands
+for language, runners in pairs(vim.fn["test#get_runners"]()) do
+	for _, runner in ipairs(runners) do
+		if vim.fn.index(vim.g["test#runner_commands"], runner) ~= -1 then
+			if vim.fn.exists(":" .. runner) == 2 then
+				goto continue
+			end
+			local runner_id = string.lower(language) .. "#" .. string.lower(runner)
+			vim.api.nvim_create_user_command(runner, function(opts)
+				vim.fn["test#execute"](runner_id, vim.split(opts.args, "%s+"))
+			end, { nargs = "*", bar = true, complete = "file" })
+		end
+		::continue::
+	end
+end
+
+-- Set project root if autochdir is enabled
+if vim.o.autochdir then
+	vim.g["test#project_root"] = vim.fn.getcwd()
+end
